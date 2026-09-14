b1819fd811c027fdd68f55ce33c56bef484a1c81
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 23:41:33 +0500

    card qoshdim

--- src/Components/Story/Story.jsx
+++ src/Components/Story/Story.jsx
@@ -15,21 +15,21 @@ const Story = () => {
                 <ul className='story-list'>
                     <li className='story-item'>
                         <img className='story-img' src={st1} alt="" />
-                        <p className='story-text2'></p>
-                        <h1 className='story-title2'></h1>
-                        <p className='story-text3'></p>
+                        <p className='story-text2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem Floyd Miles Vice President, CLI</p>
+                        <h1 className='story-title2'>Floyd Miles</h1>
+                        <p className='story-text3'>Vice President, CLI</p>
                     </li>
                     <li className='story-item'>
                         <img className='story-img' src={st2} alt="" />
-                        <p className='story-text2'></p>
-                        <h1 className='story-title2'></h1>
-                        <p className='story-text3'></p>
+                        <p className='story-text2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem Jane Cooper CEO, JPNL</p>
+                        <h1 className='story-title2'>Jane Cooper</h1>
+                        <p className='story-text3'>CEO, JPNL</p>
                     </li>
                     <li className='story-item'>
                         <img className='story-img' src={st3} alt="" />
-                        <p className='story-text2'></p>
-                        <h1 className='story-title2'></h1>
-                        <p className='story-text3'></p>
+                        <p className='story-text2'>LemonWares saved our time in Hosting my company page.</p>
+                        <h1 className='story-title2'>Kristin Watson</h1>
+                        <p className='story-text3'>Co-Founder, LeeveOn Branding</p>
                     </li>
                 </ul>
             </div>
