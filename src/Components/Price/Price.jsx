c52f0bbf92f8feeddb915122909d8440a4499d30
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 23:25:11 +0500

    cards qoshildi

--- src/Components/Price/Price.jsx
+++ src/Components/Price/Price.jsx
@@ -11,8 +11,55 @@ const Price = () => {
                 <p className='price-text'>Choose the package that suits you</p>
                 <a className='price-link' href="#">Basic</a>
                 <a className='price-link' href="#">Premium</a>
-                <ul className='price'>
-                    <li></li>
+                <ul className='price-list'>
+                    <li className='price-item'>
+                        <h1 className='price-title2'></h1>
+                        <p className='price-text2'></p>
+                        <h2 className='price-title3'></h2>
+                        <p className='price-text3'></p>
+                        <button className='price-btn'></button>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                    </li>
+                    <li className='price-item'>
+                        <h1 className='price-title2'></h1>
+                        <p className='price-text2'></p>
+                        <h2 className='price-title3'></h2>
+                        <p className='price-text3'></p>
+                        <button className='price-btn'></button>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                    </li>
+                    <li className='price-item'>
+                        <h1 className='price-title2'></h1>
+                        <p className='price-text2'></p>
+                        <h2 className='price-title3'></h2>
+                        <p className='price-text3'></p>
+                        <button className='price-btn'></button>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                        <p className='price-text4'></p>
+                    </li>
                 </ul>
             </div>
         </div>
