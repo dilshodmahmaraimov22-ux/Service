5e9e3b710e6155cdc9a7bab18833ece27c5680f5
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 23:21:47 +0500

    matn qoshildi

--- src/Components/Price/Price.jsx
+++ src/Components/Price/Price.jsx
@@ -3,7 +3,21 @@ import './Price.css'
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <>
+    <div className='price'>
+        <div className='container'>
+            <div className='price-container'>
+                <h1 className='price-title'>Ready to get started with Lemon Wares?</h1>
+                <p className='price-text'>Choose the package that suits you</p>
+                <a className='price-link' href="#">Basic</a>
+                <a className='price-link' href="#">Premium</a>
+                <ul className='price'>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
