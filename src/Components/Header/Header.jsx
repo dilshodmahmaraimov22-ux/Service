6d4ef06226b3b0386e4a19073f1bf799342976ab
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 22:33:01 +0500

    link qoshildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,8 +9,18 @@ const Header = () => {
         <div className='container'>
             <div className='header-container'>
                 <ul className='header-list'>
-                    <li>
-                        <img src={logo} alt="" />
+                    <li className='header-item'>
+                        <img className='header-img' src={logo} alt="" />
+                        <a className='header-link' href="#">Our Services</a>
+                    </li>
+                    <li className='header-item'>
+                        <a className='header-link' href="#">About</a>
+                        <a className='header-link' href="#">Blog&News</a>
+                        <a className='header-link' href="#">Contact</a>
+                    </li>
+                    <li className='header-item'>
+                        <a className='header-link' href="#">Account</a>
+                        <a className='header-link' href="#">+2349067322844</a>
                     </li>
                 </ul>
             </div>
