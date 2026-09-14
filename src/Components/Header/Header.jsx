86f2f95cc380c9fe3495791f50d9a8d02ca38214
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 22:26:36 +0500

    raam qoshildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './header.css'
+import logo from '../Images/logo.png'
 
 const Header = () => {
   return (
@@ -6,7 +8,11 @@ const Header = () => {
     <div className='header'>
         <div className='container'>
             <div className='header-container'>
-                <ul className=']'></ul>
+                <ul className='header-list'>
+                    <li>
+                        <img src={logo} alt="" />
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
