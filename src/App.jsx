f2e4f8620baf99cbc5b3aabbde975a0fc576a89c
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 23:33:38 +0500

    story qoshilid

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header'
 import Hero from './Components/Hero/Hero'
 import Reklama from './Components/Reklama/Reklama'
 import Price from './Components/Price/Price'
+import Story from './Components/Story/Story'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Hero/>
     <Reklama/>
     <Price/>
+    <Story/>
     </>
   )
 }
