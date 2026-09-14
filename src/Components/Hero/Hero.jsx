7370657f47aca2c25264da93d3f73251dff9edee
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 22:47:25 +0500

    link yaratildi

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
             <div className='hero-container'>
                 <ul className='hero-list'>
                     <li>
-                        <a className='hero-link' href="#"></a>
-                        <a className='hero-link' href="#"></a>
-                        <a className='hero-link' href="#"></a>
-                        <a className='hero-link' href="#"></a>
+                        <a className='hero-link' href="#">Hosting</a>
+                        <a className='hero-link' href="#">Domain</a>
+                        <a className='hero-link' href="#">SEO </a>
+                        <a className='hero-link' href="#">Email</a>
                     </li>
                 </ul>
             </div>
