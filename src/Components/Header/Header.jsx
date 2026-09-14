48275ebc4a9e52b2ca7d423abd41f43773bb8ea7
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 22:24:26 +0500

    header yaratildiu

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className='header'>
+        <div className='container'>
+            <div className='header-container'>
+                <ul className=']'></ul>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
