7bb20d6d989fd637d1a65fd9020218c6815789f9
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-14 23:05:30 +0500

    rasm qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Hero from './Components/Hero/Hero'
+import Reklama from './Components/Reklama/Reklama'
 
 const App = () => {
   return (
     <>
     <Header/>
     <Hero/>
+    <Reklama/>
     </>
   )
 }
--- src/Components/Reklama/Reklama.jsx
+++ src/Components/Reklama/Reklama.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Reklama.css'
+import rek1 from '../Images/Rek1.png'
+import rek2 from '../Images/Rek2.png'
+import rek3 from '../Images/Rek3.png'
+import rek4 from '../Images/Rek4.png'
+import rek5 from '../Images/Rek5.png'
+import rek6 from '../Images/Rek6.png'
 
 const Reklama = () => {
   return (
@@ -7,7 +13,20 @@ const Reklama = () => {
     <div className='reklama'>
         <div className='container'>
             <div className='reklama-container'>
-                <ul className='reklama-list'></ul>
+                <ul className='reklama-list'>
+                    <li className='reklama-item'>
+                        <h1 className='reklama-title'>True Cloud Web  Hosting</h1>
+                        <p className='reklama-text'>True Cloud Web Hosting All of the hosting packages we offer are deployed instantly on our SSD powered cloud. We don’t use dedicated servers that operate on single pieces of hardware. Our entire infrastructure is built to be reliable, secure, and scalable.</p>
+                    </li>
+                    <li className='reklama-item'>
+                        <img src={rek1} alt="rasm" />
+                        <img src={rek2} alt="rasm" />
+                        <img src={rek3} alt="rasm" />
+                        <img src={rek4} alt="rasm" />
+                        <img src={rek5} alt="rasm" />
+                        <img src={rek6} alt="rasm" />
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
