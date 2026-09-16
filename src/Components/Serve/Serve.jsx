a913cc5721aa35b03ebc2de42f0eb549c6ee936b
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-16 21:59:00 +0500

    rasm qoshildi

--- src/Components/Serve/Serve.jsx
+++ src/Components/Serve/Serve.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import './Serve.css'
+import s1 from '../Images/S1.png'
+import s2 from '../Images/S2.png'
+import s3 from '../Images/S3.png'
+import s4 from '../Images/S4.png'
+import s5 from '../Images/S5.png'
+import s6 from '../Images/S6.png'
 
 const Serve = () => {
   return (
@@ -9,7 +16,15 @@ const Serve = () => {
                 <ul className='serve-list'>
                    <li className='serve-item'>
                     <h1 className='serve'></h1>
-                    <p></p>
+                    <p className='serve-item'></p>
+                   </li>
+                   <li className='serve-item'>
+                    <img src={s1} alt="" />
+                    <img src={s2} alt="" />
+                    <img src={s3} alt="" />
+                    <img src={s4} alt="" />
+                    <img src={s4} alt="" />
+                    <img src={s5} alt="" />
                    </li>
                 </ul>
             </div>
