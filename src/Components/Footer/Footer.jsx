33db74700b42f9f804db6b68df9f8be0578ec1d8
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-16 22:33:35 +0500

    rasm qoshildi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import lemon from '../Images/Lemon.png'
 
 const Footer = () => {
   return (
     <>
+    <div className='footer'>
+        <div className='container'>
+            <div className="footer-container">
+                <ul className='footer-list'>
+                    <li className='footer-item'>
+                        <img src={lemon} alt="" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
     </>
   )
 }
