0a5eab48aba595bb6ac946c42cdf8dab0033500a
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-16 22:35:20 +0500

    linkq qoshildi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,36 @@ const Footer = () => {
             <div className="footer-container">
                 <ul className='footer-list'>
                     <li className='footer-item'>
-                        <img src={lemon} alt="" />
+                        <img className='footer-img' src={lemon} alt="" />
+                        <p className='footer-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem</p>
+                    </li>
+                    <li className='footer-item'>
+                        <a className='footer-link' href="#">Service</a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                    </li>
+                    <li className='footer-item'>
+                        <a className='footer-link' href="#">Service</a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                    </li>
+                    <li className='footer-item'>
+                        <a className='footer-link' href="#">Service</a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                    </li>
+                    <li className='footer-item'>
+                        <a className='footer-link' href="#">Service</a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#"></a>
                     </li>
                 </ul>
             </div>
