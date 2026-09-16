f8d58852bc4397aff9a0cc94ff89e212c5571bb1
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-16 22:38:46 +0500

    class berdimg

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,31 +14,33 @@ const Footer = () => {
                     </li>
                     <li className='footer-item'>
                         <a className='footer-link' href="#">Service</a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#">Domain</a>
+                        <a className='footer-link' href="#">Shared Hosting</a>
+                        <a className='footer-link' href="#">Cloud Hosting</a>
+                        <a className='footer-link' href="#">Private Hosting</a>
                     </li>
                     <li className='footer-item'>
-                        <a className='footer-link' href="#">Service</a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#">Hosting</a>
+                        <a className='footer-link' href="#">Cheap Hosting</a>
+                        <a className='footer-link' href="#">Hosting Wordpress</a>
+                        <a className='footer-link' href="#">Email Hosting</a>
+                        <a className='footer-link' href="#">Hosting Unlimited</a>
                     </li>
                     <li className='footer-item'>
-                        <a className='footer-link' href="#">Service</a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#">Company</a>
+                        <a className='footer-link' href="#">About</a>
+                        <a className='footer-link' href="#">Career</a>
+                        <a className='footer-link' href="#">Contact Us</a>
                     </li>
                     <li className='footer-item'>
-                        <a className='footer-link' href="#">Service</a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
-                        <a className='footer-link' href="#"></a>
+                        <a className='footer-link' href="#">Help</a>
+                        <a className='footer-link' href="#">FAQ</a>
+                        <a className='footer-link' href="#">Help support</a>
+                    </li>
+                </ul>
+                <ul className='footer-list2'>
+                    <li className='fotter-item2'>
+                        
                     </li>
                 </ul>
             </div>
